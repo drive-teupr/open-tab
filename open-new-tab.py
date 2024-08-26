d18f44eb56e51bacdd70ae1eb3b4fb4f3848b647
drive-teupr <drive1.teup3parepare@gmail.com>
2024-08-26 21:15:42 +0800

    Add files via upload

---
+++ open-new-tab.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from streamlit.components.v1 import html
+
+def open_page(url):
+    open_script= """
+        <script type="text/javascript">
+            window.open('%s', '_blank').focus();
+        </script>
+    """ % (url)
+    html(open_script)
+
+st.button('Open link', on_click=open_page('https://streamlit.io'))
